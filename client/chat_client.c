c31455caab5cf10e5545ddcd3aa80d206f931b32
pingulee <pigulee@soosan.co.kr>
2024-08-26 19:33:53 +0900

    포트 번호 3288로 수정 예약된 서비스가 0~1023까지 있으므로 수정

--- client/chat_client.c
+++ client/chat_client.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <winsock2.h>
 
-#define SERVER_IP "127.0.0.1" // 서버 IP 주소
-#define SERVER_PORT 8080      // 서버 포트
-#define BUFFER_SIZE 1024      // 서버 버퍼 크기
+#define SERVER_IP "172.29.133.231 " // 서버 IP 주소
+#define SERVER_PORT 3288 // 서버 포트
+#define BUFFER_SIZE 1024 // 서버 버퍼 크기
 
 int main()
 {
