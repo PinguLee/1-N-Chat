1de17e63efd555365fc20c52548254a890afba07
pingulee <pigulee@soosan.co.kr>
2024-08-29 17:45:57 +0900

    printf로 디버깅

--- chat_server.c
+++ chat_server.c
@@ -9,7 +9,7 @@
 
 int main() {
 	int server_fd;
-  int new_socket;
+	int new_socket;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
 
@@ -18,6 +18,7 @@ int main() {
 		perror("소켓 생성 실패");
 		exit(1);
 	}
+	printf("소켓 생성 성공");
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
@@ -28,12 +29,14 @@ int main() {
 		close(server_fd);
 		exit(1);
 	}
+	printf("소켓 바인딩 성공");
 
 	if (listen(server_fd, 3) < 0) {
 		perror("소켓 리스닝 실패");
 		close(server_fd);
 		exit(1);
 	}
+	printf("소켓 리스닝 성공");
 
 	new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 	if (new_socket < 0) {
@@ -41,8 +44,7 @@ int main() {
 		close(server_fd);
 		exit(1);
 	}
-
-	printf("클라이언트 연결 완료\n");
+	printf("클라이언트 연결 성공\n");
 
 	close(new_socket);
 	close(server_fd);
