18e35038ab8181f549e604c5e0f2261dc1c01bb7
pingulee <pigulee@soosan.co.kr>
2024-08-30 06:46:35 +0900

    tab -> 띄어쓰기

--- chat_server.c
+++ chat_server.c
@@ -8,45 +8,45 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-	int server_fd;
-	int new_socket;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
+    int server_fd;
+    int new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0) {
-		perror("소켓 생성 실패\n");
-		exit(1);
-	}
-	printf("소켓 생성 성공\n");
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("소켓 생성 실패\n");
+        exit(1);
+    }
+    printf("소켓 생성 성공\n");
 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-		perror("소켓 바인딩 실패\n");
-		close(server_fd);
-		exit(1);
-	}
-	printf("소켓 바인딩 성공\n");
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("소켓 바인딩 실패\n");
+        close(server_fd);
+        exit(1);
+    }
+    printf("소켓 바인딩 성공\n");
 
-	if (listen(server_fd, 3) < 0) {
-		perror("소켓 리스닝 실패\n");
-		close(server_fd);
-		exit(1);
-	}
-	printf("소켓 리스닝 성공\n");
+    if (listen(server_fd, 3) < 0) {
+        perror("소켓 리스닝 실패\n");
+        close(server_fd);
+        exit(1);
+    }
+    printf("소켓 리스닝 성공\n");
 
-	new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-	if (new_socket < 0) {
-		perror("클라이언트 연결 실패\n");
-		close(server_fd);
-		exit(1);
-	}
-	printf("클라이언트 연결 성공\n");
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (new_socket < 0) {
+        perror("클라이언트 연결 실패\n");
+        close(server_fd);
+        exit(1);
+    }
+    printf("클라이언트 연결 성공\n");
 
-	close(new_socket);
-	close(server_fd);
-	return 0;
+    close(new_socket);
+    close(server_fd);
+    return 0;
 }
