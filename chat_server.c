c4125cc17b34c9641e6b480fa35559584467bea4
pingulee <pigulee@soosan.co.kr>
2024-08-29 19:56:49 +0900

    줄바꿈처리

--- chat_server.c
+++ chat_server.c
@@ -15,32 +15,32 @@ int main() {
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
-		perror("소켓 생성 실패");
+		perror("소켓 생성 실패\n");
 		exit(1);
 	}
-	printf("소켓 생성 성공");
+	printf("소켓 생성 성공\n");
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-		perror("소켓 바인딩 실패");
+		perror("소켓 바인딩 실패\n");
 		close(server_fd);
 		exit(1);
 	}
-	printf("소켓 바인딩 성공");
+	printf("소켓 바인딩 성공\n");
 
 	if (listen(server_fd, 3) < 0) {
-		perror("소켓 리스닝 실패");
+		perror("소켓 리스닝 실패\n");
 		close(server_fd);
 		exit(1);
 	}
-	printf("소켓 리스닝 성공");
+	printf("소켓 리스닝 성공\n");
 
 	new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 	if (new_socket < 0) {
-		perror("클라이언트 연결 실패");
+		perror("클라이언트 연결 실패\n");
 		close(server_fd);
 		exit(1);
 	}
