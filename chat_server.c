d7d7e0cfd35cb3128a50fffdbd5d9ad2407c4b7f
pingulee <pigulee@soosan.co.kr>
2024-08-30 12:01:32 +0900

    Win32 스레드를 이용한 비동기 입력 처리

--- chat_server.c
+++ chat_server.c
@@ -1,124 +1,105 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/select.h>
+#include <winsock2.h>
+#include <windows.h>
 
 #define PORT 3288
 #define BUFFER_SIZE 1024
-#define MAX_CLIENTS 16
 
-typedef struct {
-    int socket;
-    char ip[INET_ADDRSTRLEN];
-} Client;
+#pragma comment(lib, "ws2_32.lib")
+
+// 전역 변수
+SOCKET clientSocket;
+
+// 입력을 처리할 스레드 함수
+DWORD WINAPI InputThread(LPVOID lpParam) {
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        printf("입력: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        send(clientSocket, buffer, strlen(buffer), 0);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+    return 0;
+}
+
+// 수신을 처리할 스레드 함수
+DWORD WINAPI ReceiveThread(LPVOID lpParam) {
+    char buffer[BUFFER_SIZE];
+    int recv_len;
+    while (1) {
+        recv_len = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+        if (recv_len == SOCKET_ERROR) {
+            printf("수신 오류: %d\n", WSAGetLastError());
+            break;
+        } else if (recv_len == 0) {
+            printf("서버와의 연결이 종료되었습니다.\n");
+            break;
+        }
+        buffer[recv_len] = '\0';
+        printf("서버로부터 받은 메시지: %s\n", buffer);
+    }
+    return 0;
+}
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    fd_set readfds;
-    int max_sd, sd, activity;
-    char buffer[BUFFER_SIZE];
-    Client clients[MAX_CLIENTS];
-    int client_count = 0;
+    WSADATA wsaData;
+    HANDLE hInputThread, hReceiveThread;
+    DWORD threadId;
 
-    // 클라이언트 배열 초기화
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        clients[i].socket = 0;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("소켓 초기화 실패: %d\n", WSAGetLastError());
+        exit(1);
     }
+    printf("소켓 초기화 성공\n");
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("소켓 생성 실패");
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        printf("소켓 생성 실패: %d\n", WSAGetLastError());
+        WSACleanup();
         exit(1);
     }
     printf("소켓 생성 성공\n");
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("172.29.133.231");
+    serverAddress.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("소켓 바인딩 실패");
-        close(server_fd);
+    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        printf("서버 연결 실패: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
         exit(1);
     }
-    printf("소켓 바인딩 성공\n");
+    printf("서버 연결 성공\n");
 
-    if (listen(server_fd, 16) < 0) {
-        perror("소켓 리스닝 실패");
-        close(server_fd);
+    // 입력 스레드 생성
+    hInputThread = CreateThread(NULL, 0, InputThread, NULL, 0, &threadId);
+    if (hInputThread == NULL) {
+        printf("입력 스레드 생성 실패: %d\n", GetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
         exit(1);
     }
-    printf("소켓 리스닝 성공\n");
 
-    while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = clients[i].socket;
-            if (sd > 0) {
-                FD_SET(sd, &readfds);
-            }
-            if (sd > max_sd) {
-                max_sd = sd;
-            }
-        }
-
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-        if (activity < 0) {
-            perror("select 오류");
-            continue;
-        }
-
-        if (FD_ISSET(server_fd, &readfds)) {
-            new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-            if (new_socket < 0) {
-                perror("클라이언트 연결 실패");
-                continue;
-            }
-
-            // 클라이언트 IP 주소 가져오기
-            char client_ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &address.sin_addr, client_ip, sizeof(client_ip));
-
-            printf("클라이언트 연결 성공: %s\n", client_ip);
-
-            if (client_count < MAX_CLIENTS) {
-                clients[client_count].socket = new_socket;
-                strncpy(clients[client_count].ip, client_ip, sizeof(clients[client_count].ip));
-                client_count++;
-            } else {
-                printf("클라이언트 수 초과\n");
-                close(new_socket);
-            }
-        }
-
-        for (int i = 0; i < MAX_CLIENTS; i++) {
-            sd = clients[i].socket;
-            if (FD_ISSET(sd, &readfds)) {
-                int read_data = read(sd, buffer, BUFFER_SIZE);
-                if (read_data <= 0) {
-                    close(sd);
-                    clients[i].socket = 0;
-                } else {
-                    buffer[read_data] = '\0';
-                    printf("%s: %s\n", clients[i].ip, buffer);
-
-                    for (int j = 0; j < MAX_CLIENTS; j++) {
-                        if (clients[j].socket != 0 && clients[j].socket != sd) {
-                            send(clients[j].socket, buffer, read_data, 0);
-                        }
-                    }
-                }
-            }
-        }
+    // 수신 스레드 생성
+    hReceiveThread = CreateThread(NULL, 0, ReceiveThread, NULL, 0, &threadId);
+    if (hReceiveThread == NULL) {
+        printf("수신 스레드 생성 실패: %d\n", GetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
+        exit(1);
     }
 
-    close(server_fd);
+    // 스레드가 끝날 때까지 기다림
+    WaitForSingleObject(hInputThread, INFINITE);
+    WaitForSingleObject(hReceiveThread, INFINITE);
+
+    // 자원 정리
+    closesocket(clientSocket);
+    WSACleanup();
     return 0;
 }
